0a84e9e1dabcdaeda4466ebc5b680f265aeae361
Kyle Gilbert <kyleg@fixdapp.com>
2023-08-21 23:43:55 -0700

    fix ios build issues

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'dart:async';
 import 'package:anywhere_coding_exercise/api/api_manager.dart';
 import 'package:anywhere_coding_exercise/app.dart';
 import 'package:anywhere_coding_exercise/bloc/characters/characters_bloc.dart';
+import 'package:anywhere_coding_exercise/flavors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 FutureOr<void> main() async {
   ApiManager().init();
+  F.appFlavor ??= Flavor.simpsons;
   runApp(
     BlocProvider(
       create: (context) => CharactersBloc()..add(LoadCharacters()),
